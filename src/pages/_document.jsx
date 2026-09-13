c298cf4fa749511c502abd65d826eade865e42ac
Rafael Antunes <me@rafaelgaspar.xyz>
2026-09-13 23:52:18 +0200

    feat(cacheable-custom-css): bundle custom CSS/JS via webpack

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,8 +6,6 @@ export default function Document() {
       <Head>
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/site.webmanifest?v=4" crossOrigin="use-credentials" />
-        <link rel="preload" href="/api/config/custom.css" as="style" />
-        <link rel="stylesheet" href="/api/config/custom.css" /> {/* eslint-disable-line @next/next/no-css-tags */}
       </Head>
       <body>
         <Main />
