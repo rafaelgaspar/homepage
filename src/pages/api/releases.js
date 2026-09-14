04cac0f974cb38fcdd15cc5cde5d4c0082734d2b
Rafael Antunes <me@rafaelgaspar.xyz>
2026-09-14 23:24:33 +0200

    feat(scaling-toggle): KEDA always-on toggle API and service card control

--- src/pages/api/releases.js
+++ src/pages/api/releases.js
@@ -1,9 +1,15 @@
+import { getSettings } from "utils/config/config";
 import createLogger from "utils/logger";
 import { cachedRequest } from "utils/proxy/http";
 
 const logger = createLogger("releases");
 
 export default async function handler(req, res) {
+  const settings = getSettings() ?? {};
+  if (settings.hideVersion || settings.disableUpdateCheck) {
+    return res.send([]);
+  }
+
   const releasesURL = "https://api.github.com/repos/gethomepage/homepage/releases";
   try {
     return res.send(await cachedRequest(releasesURL, 5));
